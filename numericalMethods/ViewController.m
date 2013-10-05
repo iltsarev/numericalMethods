c9af48402ac6905bb7ceb715c47492eb4e50cf17
Ilya <macbookpro@Ilyas-MBP.local>
2013-10-06 02:37:11 +0400

    Сделаны аппроксимации для явной схемы, необходимо сделать отдельные ф-ии для каждой из аппроксимаций. Они отлажены.

--- numericalMethods/ViewController.m
+++ numericalMethods/ViewController.m
@@ -54,7 +54,22 @@ void solve_tridiagonal_in_place_destructive(double x[], const size_t N, const do
         x[i] = x[i] - c[i] * x[i + 1];
 }
 
-double **explicitScheme(int K, int N, double a, double b, double c, double tau, double h){
+double phi(double x){
+    return sin(x);//x+sin(M_PI*x);
+}
+
+double phi_0(double t){
+    return exp(-t);//0;
+}
+double phi_l(double t){
+    return -exp(-t);//1;
+}
+
+double f(double x, double t){
+    return 0;
+}
+
+double **explicitScheme(int K, int N, double a, double b, double c, double tau, double h, double alpha, double betta, double gamma, double delta){
     double **U = (double **)malloc(K * sizeof(double *));
     
     for (int i = 0; i < K; i++)
@@ -64,76 +79,55 @@ double **explicitScheme(int K, int N, double a, double b, double c, double tau, 
         U[0][i] = phi(i*h);
     
     for (int k = 0; k < K - 1; ++k) {
-        U[k+1][0] = phi_0(k * tau);
         for (int i = 1; i < N; ++i) {
             U[k+1][i] = U[k][i] + a*tau/h/h*(U[k][i+1] - 2*U[k][i] + U[k][i-1]) + b*tau/2/h*(U[k][i+1] - U[k][i-1]) + c*tau*U[k][i] + tau*f(i * h, k * tau);
         }
-        U[k+1][N] = phi_l(k * tau);
+        
+      //  двухточечная первого
+      //  U[k+1][0] = -alpha/h/(betta - alpha/h)*U[k+1][1]  + phi_0((k + 1) * tau)/(betta - alpha/h);
+      //  U[k+1][N] = gamma/h/(delta + gamma/h)*U[k+1][N-1] + phi_l((k + 1) * tau)/(delta + gamma/h);
+
+      //  трехточечная второго
+      //  U[k+1][0] = alpha/2/h/(betta - 3*alpha/2/h)*(U[k+1][2] - 4*U[k+1][1]) + phi_0((k+1) * tau)/(betta - 3*alpha/2/h);
+      //  U[k+1][N] = gamma/2/h/(delta - 3*gamma/2/h)*(4*U[k+1][N-1] - U[k+1][N-2]) + phi_l((k+1) * tau)/(delta - 3*gamma/2/h);
+
+      //  Двухточечная второго
+          U[k+1][0] = 1/(2*a*alpha/h + h*alpha/tau - c*h*alpha - betta*(2*a - b*h))*(U[k+1][1]*2*a*alpha/h + U[k][0]*h*alpha/tau - phi_0((k+1) * tau)*(2*a-b*h)+f(0, (k+1)*tau)*h*alpha);
+          U[k+1][N] = 1/(2*a*gamma/h + h*gamma/tau - c*h*gamma + delta*(2*a + b*h))*(U[k+1][N-1]*2*a*gamma/h + U[k][N]*h*gamma/tau + phi_l((k+1) * tau)*(2*a+b*h)+f(N, (k+1)*tau)*h*gamma);
     }
     return U;
 }
 
-double phi(double x){
-    return x+sin(M_PI*x);
-}
-
-double phi_0(double t){
-    return 0;
-}
-double phi_l(double t){
-    return 1;
-}
-
-double f(double x, double t){
-    return 0;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
+    int K = 1000;
+    int N = 10;
     
-    double a = 1, b = 0,c = 0, tau = 0.001,h = 0.1;
-    double l = 1;;
-    int K = 10;
-    int N = l / h;
-    double alpha = 0.0, betta = 1.0, gamma = 0.0, delta = 1.0;
-//    double phi_0 = 0;
-//    double phi_l = 1;
+    double l = 3.14; //1
+    double T = 10;
     
-    double sigma = a*tau/h/h;
-    if(sigma > 0.5){
-        printf("Sigma in not good. (< 0.5)\n");
+    double tau = T / K, h = l / N;
+    
+    double a = 1, b = 0, c = 0;
+    double alpha = 1.0, betta = 0.0, gamma = 1.0, delta = 0.0;//double alpha = 0.0, betta = 1.0, gamma = 0.0, delta = 1.0;
+    
+    if(a*tau/h/h > 0.5){
+        printf("bad sigma");
         exit(0);
     }
     
-    double **U = explicitScheme(K, N, a, b, c, tau, h);
+    double **U = explicitScheme(K, N, a, b, c, tau, h, alpha, betta, gamma, delta);
     
     for (int i = 0; i < K; ++i) {
+        printf("K = %f\n", i*tau);
         for (int j = 0; j <= N; ++j) {
-            printf("%f ", U[i][j]);
+            printf("%f	%f\n", j*h, U[i][j]);
         }
-        printf("\n");
+        printf("\n\n\n");
     }
-    
-    printf("\nАпроксимация двухточечная первого порядка:\n\n");    //Двухточечная 1го порядка (вроде работает, проверял на 4ом варианте)
-    
-    double s = a*a*tau/h/h;
-    for (int k = 0; k < K - 1; ++k) {
-        for (int i = 1; i < N; ++i) {
-            U[k+1][i] = s*U[k][i+1] + (1-2*s)*U[k][i] + s*U[k][i-1];
-        }
-        U[k+1][0] = -alpha/h/(betta - alpha/h)*U[k+1][1]  + phi_0(k * tau)/(betta - alpha/h);
-        U[k+1][N] = gamma/h/(delta + gamma/h)*U[k+1][N-1] + phi_l(k * tau)/(delta + gamma/h);
-    }
-
-    for (int i = 0; i < K; ++i) {
-        for (int j = 0; j <= N; ++j) {
-            printf("%f ", U[i][j]);
-        }
-        printf("\n");
-    }
-    
 }
 
 - (void)didReceiveMemoryWarning
